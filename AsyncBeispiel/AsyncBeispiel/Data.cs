209bc48ee60d6bc36931c5cb0b1fcc16fd726015
drjofu <drjofu@web.de>
2024-07-05 15:03:52 +0200

    Tag 3

---
+++ AsyncBeispiel/AsyncBeispiel/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncBeispiel
+{
+  public class Data : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public int Counter { get; set; }
+    private readonly Timer timer;
+
+    public Data()
+    {
+      timer = new Timer(TimerTick, null, 1000, 1000);
+    }
+
+    private void TimerTick(object? state)
+    {
+      Counter++;
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Counter)));
+    }
+  }
+}
